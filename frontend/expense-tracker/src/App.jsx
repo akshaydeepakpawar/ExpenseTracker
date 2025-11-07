a3f2064bfc6299af1d1ef61e5d9f25753657f6be
akshay pawar <ap3690700@gmail.com>
2025-11-07 23:11:57 +0530

    day 1 -> I created the half front-end for login page.

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Home/>}/>
+          <Route path='/income' element={<Income/>}/>
+          <Route path='/expense' element={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+
+const Root=()=>{
+  //check if token existes in localStorage
+  const isAuthenticated = !! localStorage.getItem("token");
+  //redirect to dashboard if authenticated otherwise to login 
+  return isAuthenticated? (
+    <Navigate to="/dashboard"/>
+  ): (<Navigate to="/login"/>);
+}
